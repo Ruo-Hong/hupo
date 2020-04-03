15463d77601b7020c1b6d178a5a3870edc3fea1d
“ <yapeng4321@163.com>
2020-04-03 13:47:51 +0800

    test

---
+++ dome01.py
@@ -0,0 +1,45 @@
+'''print("hello world") #字符串
+print(2333) #整数
+print(2.2) #小数
+print(True) #布尔类型
+print(()) #元组
+print([]) #数组
+print({}) #字典
+print("哈哈",233,2.33)
+print("哈哈"+"嘿嘿")
+print("哈哈"*10)
+print(1+1==2)
+a = "张三"
+print(a)'''
+
+
+
+'''a = input("请输入：") #定义一个变量a
+print("input获取内容：",a) #输出变量a的值'''
+
+# a = int(input("请输入："))
+# b = int(input("请输入："))
+# print("结果：",a+b)
+
+
+
+'''a = input("请输入：")
+b = input("请输入：")
+print("结果：",len(a+b))
+'''
+
+
+'''a = input("请输入：")
+print("输出结果：",a)'''
+
+# print(type(123))
+
+'''a = input("请输入：")
+print("a长度有是：",len(a))'''
+
+'''a = float(input('请输入：'))
+b = float(input('请输入：'))
+print('a+b的和是：',a+b)'''
+
+
+
